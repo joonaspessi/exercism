7d77858a5e2206580d0a1306c1a3ceec973f5b77
Joonas Pessi <joonas.pessi@gmail.com>
2022-01-30 16:10:11 +0200

    assembly line

--- rust/assembly-line/src/lib.rs
+++ rust/assembly-line/src/lib.rs
@@ -3,9 +3,29 @@
 #![allow(unused)]
 
 pub fn production_rate_per_hour(speed: u8) -> f64 {
-    unimplemented!("calculate hourly production rate at speed: {}", speed)
+    let production_rate_per_hour = 221.0;
+    let mut success_rate = 1.0;
+    if speed > 4 && speed <= 8 {
+        success_rate = 0.9;
+    }
+    if speed > 8 {
+        success_rate = 0.77;
+    }
+
+    production_rate_per_hour * success_rate * speed as f64
 }
 
 pub fn working_items_per_minute(speed: u8) -> u32 {
-    unimplemented!("calculate the amount of working items at speed: {}", speed)
+    let production_rate_per_minute = 221.0 / 60.0;
+    let mut success_rate = 1.0;
+    if speed > 4 && speed <= 8 {
+        success_rate = 0.9;
+    }
+    if speed > 8 {
+        success_rate = 0.77;
+    }
+
+    let working_items = production_rate_per_minute * success_rate * (speed as f64);
+
+    working_items.floor() as u32
 }
