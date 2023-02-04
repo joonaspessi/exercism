9fd25e63ee9e85fec8cd040668ca427de6cbc3a8
Joonas Pessi <joonas.pessi@cargotec.com>
2023-02-04 16:16:32 +0200

    go bird-watcher

---
+++ go/bird-watcher/bird_watcher.go
@@ -0,0 +1,36 @@
+package birdwatcher
+
+// TotalBirdCount return the total bird count by summing
+// the individual day's counts.
+func TotalBirdCount(birdsPerDay []int) int {
+	count := 0
+	for i := 0; i < len(birdsPerDay); i++ {
+		count += birdsPerDay[i]
+	}
+	return count
+}
+
+// BirdsInWeek returns the total bird count by summing
+// only the items belonging to the given week.
+func BirdsInWeek(birdsPerDay []int, week int) int {
+	var weeklyCount int = 1
+	count := 0
+	for i := 0; i < len(birdsPerDay); i++ {
+		if weeklyCount == week {
+			count += birdsPerDay[i]
+		}
+		if i != 0 && i%7 == 0 {
+			weeklyCount += 1
+		}
+	}
+	return count
+}
+
+// FixBirdCountLog returns the bird counts after correcting
+// the bird counts for alternate days.
+func FixBirdCountLog(birdsPerDay []int) []int {
+	for i := 0; i < len(birdsPerDay); i += 2 {
+		birdsPerDay[i] += 1
+	}
+	return birdsPerDay
+}
